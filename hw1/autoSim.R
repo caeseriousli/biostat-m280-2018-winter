deccf2113911c90007223af8880eee841cc780db
Caesar (Zexuan) Li <lizexuan@g.ucla.edu>
2018-01-26 09:10:36 +0000

    Modified autoSim.R for HW1 Q3 #2
    - Added an extra for loop iterating through distributions within the original for loop iterating through different sample sizes
    
    - Passing additional arugments (including iterating distributions) necessary for runSimR.R
    
    About to test the code (in order to create less files for the test run I'm about to change nVals to seq(400, 500, by = 100). so it should be only 2*3=6 files created. Will change it back before submission

--- hw1/autoSim.R
+++ hw1/autoSim.R
@@ -1,10 +1,13 @@
 # autoSim.R
 
 nVals = seq(100, 500, by=100)
+distTypes = c("gaussian", "t1", "t5")
 for (n in nVals) {
-  oFile = paste("n", n, ".txt", sep="")
-  arg = paste("n=", n, sep="")
-  sysCall = paste("nohup Rscript runSim.R ", arg, " > ", oFile)
-  system(sysCall)
-  print(paste("sysCall=", sysCall, sep=""))
+  for (distr in distTypes) {
+    oFile = paste("n", n, "_", distr, ".txt", sep="")
+    arg = paste("seed=280 ", "n=", n, " dist=", distr, " rep=50", sep = "")
+    sysCall = paste("nohup Rscript runSim.R ", arg, " > ", oFile, sep = "")
+    system(sysCall)
+    print(paste("sysCall=", sysCall, sep=""))
+  }
 }
