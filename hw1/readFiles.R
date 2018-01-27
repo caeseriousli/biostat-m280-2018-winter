e4a86858f2f196654a8c76df5bb927028d6295ee
Caesar (Zexuan) Li <lizexuan@g.ucla.edu>
2018-01-27 09:23:57 +0000

    Changed code format in the Rmarkdown solution file to not exceed 80 characters per line.
    - Test ran Rmarkdown and everything seems good
    
    - Will consult professor regarding last output table format on Tuesday. After that will be ready to merge to master branch and tag submission

--- hw1/readFiles.R
+++ hw1/readFiles.R
@@ -10,8 +10,11 @@ colnames(finalTable) = c("n", "Method", "t1", "t5", "Gaussian")
 finalTable = as.data.frame(finalTable)
 
 for (n in nVals) {
+  # create a temperary table for each sample size n to store values read from files
   tempTable = matrix(data = NA, nrow = 2, ncol = 5)
   colnames(tempTable) = c("n", "Method", "t1", "t5", "Gaussian")
+  # set up a column index counter (iterator) to help input value to the temp table
+  # it iterates from 3 to 5 and back to 3, to fill in values from 3 distributions
   counter = 3
   for (distr in distTypes) {
     # "Borrowing" code from autoSim.R to read in relevant files
@@ -22,6 +25,7 @@ for (n in nVals) {
     tempTable[, 1] = n
     tempTable[, 2] = reading[, 1]
     tempTable[, counter] = reading[, 2]
+    # counter +1 so the next file it reads, values will be stored in next column
     counter = counter + 1
   }
   finalTable = rbind(finalTable, tempTable)
